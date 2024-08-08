5f8013e94c9b3023c0bd31be8595dc1f95502ad0
ranitchakraborty1 <162864925+ranitchakraborty1@users.noreply.github.com>
2024-08-08 11:09:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your JS code here. If required.
+   const inputs = document.querySelectorAll('.code');
+
+        // Add event listeners to each input field
+        inputs.forEach((input, index) => {
+            input.addEventListener('input', (event) => {
+                if (event.target.value.length === 1) {
+                    if (index < inputs.length - 1) {
+                        inputs[index + 1].focus();
+                    }
+                }
+            });
+
+            input.addEventListener('keydown', (event) => {
+                if (event.key === 'Backspace' && event.target.value === '') {
+                    if (index > 0) {
+                        inputs[index - 1].focus();
+                    }
+                }
+            });
+        });
+
  
